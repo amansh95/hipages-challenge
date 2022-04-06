18a41c1f764f9a96c58c559c3692023d6546aec0
Aman <aman.shekhar1995@gmail.com>
2022-04-06 23:48:08 +1000

    Fixed tab reset

--- ui/src/Tabs.js
+++ ui/src/Tabs.js
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Tabs = ({ children }) => {
-  const [active, setActive] = useState(children[0].props.label);
+  const tabKey = "currentTab";
+  const [active, setActive] = useState();
+
+  useEffect(() => {
+    const storedTab = localStorage.getItem(tabKey);
+    if (storedTab) setActive(storedTab);
+    else setActive(children[0].props.label);
+  }, []);
 
   const handleClick = (e, tabName) => {
     e.preventDefault();
     setActive(tabName);
+    localStorage.setItem(tabKey, tabName);
   };
   return (
     <div>
       <ul className="tabs">
         {children.map((tab) => (
           <li
-            className={(tab.props.label == active ? "current" : "")}
+            className={tab.props.label == active ? "current" : ""}
             key={tab.props.label}
           >
             <a href="#" onClick={(e) => handleClick(e, tab.props.label)}>
@@ -22,7 +30,7 @@ const Tabs = ({ children }) => {
         ))}
       </ul>
       {children.map((val) => {
-        if ((val.props.label == active))
+        if (val.props.label == active)
           return (
             <div key={val.props.label}>
               <p>{val}</p>

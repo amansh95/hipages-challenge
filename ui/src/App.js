f3ac737a0957b6acc04a904a0544b64044e0b446
Aman <aman.shekhar1995@gmail.com>
2022-04-06 22:27:30 +1000

    Moved the code into more understandable components

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
-import LeadsApp from './Leads';
+import axios from 'axios'
+import LeadsList from './LeadsList';
+import { endpointPrefix } from "./Lead"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LeadsApp/>
-      </div>
-    );
+function App() {
+
+  const [invited, setInvited] = useState([])
+  const [accepted, setAccepted] = useState([])
+
+  useEffect(() => {
+    fetchInvited()
+  }, [])
+
+  const fetchInvited = () => {
+    axios.get(`${endpointPrefix}/leads/invited`)
+      .then((res) => {
+        setInvited(res.data.invited)
+      }).catch((err) => {
+        console.log(err);
+      })
   }
+
+  useEffect(() => {
+    fetchAccepted()
+  }, [])
+
+  const fetchAccepted = () => {
+    axios.get(`${endpointPrefix}/leads/accepted`)
+      .then((res) => {
+        setAccepted(res.data.accepted)
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <div className="App">
+      <LeadsList invited={invited} accepted={accepted} />
+    </div>
+  );
 }
 
 export default App;
---
+++ ui/src/LeadsList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Lead, { endpointPrefix } from './Lead'
+import axios from 'axios'
+import { uuid } from 'uuidv4';
+
+export default function LeadsList({ invited, accepted }) {
+
+    const handlReset = async () => {
+        await axios.delete(`${endpointPrefix}/leads/all`)
+        window.location.reload(false);
+    }
+
+    return (
+        <div className="float-container">
+            <div className="float-child">
+                <h1>Invited Leads</h1>
+                <div className='item-container'>
+                    {invited.map(lead => {
+                        return <Lead key={uuid()} lead={lead} invited={true} />
+                    })
+                    }
+                </div>
+            </div>
+            <div>
+                <h1>Accepted Leads</h1>
+                <div className='item-container'>
+                    {accepted.map(lead => {
+                        return <Lead key={uuid()} lead={lead} invited={false} />
+                    })
+                    }
+                </div>
+            </div>
+            <div className="float-child-2">
+                <button className="button-1" onClick={() => handlReset()}>Reset all leads to new</button>
+            </div>
+        </div>
+    );
+}
